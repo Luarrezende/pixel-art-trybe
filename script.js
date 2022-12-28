05f6a23b89051a7f63e58ca82103e84b8a694cce
Luar <luarrezende@gmail.com>
2022-12-28 17:48:35 -0300

    alterando cores

--- script.js
+++ script.js
@@ -2,9 +2,22 @@ function generateColors() {
     const randomColors = "0123456789ABCDEF"
     let color = "#"
 
-    for (let index = 0; index < 3; index += 1) {
+    for (let index = 0; index < 6; index += 1) {
         color += randomColors.charAt(Math.floor(Math.random() * randomColors.length))
     }
     return color;
-}
-console.log(generateColors());
+} 
+
+document.getElementById("button-random-color").addEventListener("click", () =>{
+    for (let index = 1; index <= 4; index += 1) {
+        let element = document.getElementById(`color${index}`);
+        element.style.backgroundColor = generateColors();
+    }
+} )
+
+    document.getElementById("clear-board").addEventListener("click", () =>{
+        for (let index = 1; index <= 4; index += 1) {
+            let element = document.getElementById(`color${index}`);
+            element.style.backgroundColor = "white";
+        }
+})
