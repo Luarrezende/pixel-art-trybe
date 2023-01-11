d2a709a35cf5480c384127a4acf957df039efc7e
Luar <luarrezende@gmail.com>
2023-01-10 22:39:39 -0300

    criando quadro 5x5

--- script.js
+++ script.js
@@ -14,16 +14,16 @@ function generateColors() {
     return color;
 }
 
-function totalQ() {
-    for (let index = 1; index = 25; index += 1) {
-        const element = document.createElement("li");
-        element.style.height = "40px";
-        element.style.width = "40px";
-        element.style.border = "black solid 1px";
-        element.style.backgroundColor = "white";
-        element.style.display = "inline-block";
-        let board = document.getElementById("board");
-        board.appendChild(element);
+let board = document.getElementById("pixel-board");
+    for (let index = 1; index <= 25; index += 1) {
+        let lista = document.createElement("div");
+        lista.style.height = "40px";
+        lista.style.width = "40px";
+        lista.style.border = "black solid 1px";
+        lista.style.backgroundColor = "white";
+        lista.style.display = "inline-block";
+        console.log(lista);
+        board.appendChild(lista);
         if (index === 5) {
             let quebra = document.createElement("br")
             board.appendChild(quebra);
@@ -38,23 +38,28 @@ function totalQ() {
             board.appendChild(quebra);
         }
     }
-}
+
 
 document.getElementById("button-random-color").addEventListener("click", () => {
-    for (let index = 1; index <= 4; index += 1) {
-        let element = document.getElementById(`color${index}`);
-        element.style.backgroundColor = generateColors();
-        if (color2 === color1 || color3 || color4) {
-            color2.style.backgroundColor = generateColors();
-        } else if (color3 === color1 || color2 || color4) {
-            color3.style.backgroundColor = generateColors();
-        }  else if (color4 === color1 || color3 || color2) {
-            color4.style.backgroundColor = generateColors();
-        }
+    let nome = document.querySelectorAll(".color")
+    for (let index = 0; index < nome.length; index += 1) {
+        let element = nome[index]
+        element.style.backgroundColor = generateColors(); 
     }
-    color1.style.backgroundColor = "black";  
+    // console.log(color2.style.backgroundColor);
+    if (color2.style.backgroundColor === color1.style.backgroundColor || color3.style.backgroundColor || color4.style.backgroundColor) {
+        color2.style.backgroundColor = generateColors();
+    } else if (color3.style.backgroundColor === color1.style.backgroundColor || color2.style.backgroundColor || color4.style.backgroundColor) {
+        color3.style.backgroundColor = generateColors();
+    }  else if (color4.style.backgroundColor === color1.style.backgroundColor || color3.style.backgroundColor || color2.style.backgroundColor) {
+        color4.style.backgroundColor = generateColors();
+    } else if (color1 === color2 || color3 || color4) {
+        color1.style.backgroundColor = "black";
+    }
+    color1.style.backgroundColor = "black"; 
     }
 )
+ 
 
 
 document.getElementById("clear-board").addEventListener("click", () => {
